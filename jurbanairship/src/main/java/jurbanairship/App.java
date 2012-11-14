0c4ddc27b58787534786b6f9b8780f7c218eee31
kongxx <kongxx@gmail.com>
2012-11-14 10:08:53 +0800

    init for devices

--- jurbanairship/src/main/java/jurbanairship/App.java
+++ jurbanairship/src/main/java/jurbanairship/App.java
@@ -1,13 +1,40 @@
 package jurbanairship;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//		System.out.println(new File("/").getAbsolutePath());
+//		System.out.println(new File(".").getAbsolutePath());
+//    }
+
+	private static App app = new App();
+
+	private List<String> ids = new ArrayList<String>();
+
+	private App() {
+
+	}
+
+	public static App getInstance() {
+		return app;
+	}
+
+	public List<String> getIds() {
+		return ids;
+	}
+
+	public void addId(String id) {
+		ids.add(id);
+	}
+
+	public void removeId(String id) {
+		ids.remove(id);
+	}
+
 }
