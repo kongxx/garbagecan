20abfc8ec486a0c5af136c2c231e9e02f5cd448d
U-fanbin\Fanbin Kong <Fanbin Kong@fanbin.(none)>
2014-02-25 13:03:41 +0800

    -

--- chrome-extensions/douping/func.js
+++ chrome-extensions/douping/func.js
@@ -1,9 +1,8 @@
 
-var eDiv = document.getElementById('detail_bullets_id').innerHTML;
-var reg = /<li><b>ISBN:<\/b>(\s*)(\d*)(,?)(\s*)(\d*)<\/li>/;
-var result =  reg.exec(eDiv);
+var div = document.getElementById('detail_bullets_id').innerHTML;
+var reg = /<li><b>ISBN:<\/b>(\s*)(\w*)(,?)(\s*)(\w*)<\/li>/;
+var result =  reg.exec(div);
 if (result != null && result.length == 6) {
-	result.length;
 	if (result[5] != null && result[5].length > 0) {
 		isbn = result[5];
 	} else if (result[2] != null && result[2].length > 0) {
