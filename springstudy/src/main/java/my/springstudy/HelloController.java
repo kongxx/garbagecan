69606ac617bd11b5bd576a1801655ba25f989e8a
kongxx <kongxx@gmail.com>
2014-11-17 20:19:36 +0800

    spring mvc helloworld

---
+++ springstudy/src/main/java/my/springstudy/HelloController.java
@@ -0,0 +1,19 @@
+package my.springstudy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	//http://localhost:8080/springstudy/hello/
+	//http://localhost:8080/springstudy/hello/?name=kongxx
+	@RequestMapping("/hello")
+	public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
+}
