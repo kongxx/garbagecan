53c880ad1bfa66c912f87423246e95fd322d6c80
kongxx <kongxx@gmail.com>
2014-11-20 21:43:24 +0800

    init hadoopstudy

---
+++ hadoopstudy/src/main/java/my/hadoopstudy/App.java
@@ -0,0 +1,13 @@
+package my.hadoopstudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
