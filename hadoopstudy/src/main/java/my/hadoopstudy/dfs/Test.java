151838b62ac4e076f79c7271b90a76259e285d77
kongxx <kongxx@gmail.com>
2014-12-04 19:16:47 +0800

    test hdfs api

--- hadoopstudy/src/main/java/my/hadoopstudy/dfs/Test.java
+++ hadoopstudy/src/main/java/my/hadoopstudy/dfs/Test.java
@@ -1,6 +1,7 @@
-package my.hadoopstudy;
+package my.hadoopstudy.dfs;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -11,15 +12,20 @@ import java.net.URI;
 
 public class Test {
 	public static void main(String[] args) throws Exception {
-		String uri = "hdfs://9.111.254.99:9000/";
+		String uri = "hdfs://9.111.254.189:9000/";
 		Configuration config = new Configuration();
 		FileSystem fs = FileSystem.get(URI.create(uri), config);
-		FileStatus[] statuses = fs.listStatus(new Path("/user/fkong/input"));
+		FileStatus[] statuses = fs.listStatus(new Path("/user/fkong"));
 		for (FileStatus status : statuses) {
 			System.out.println(status);
 		}
 
-		InputStream is = fs.open(new Path("/user/fkong/input/slaves"));
+		FSDataOutputStream os = fs.create(new Path("/user/fkong/test.log"));
+		os.write("Hello World!".getBytes());
+		os.flush();
+		os.close();
+
+		InputStream is = fs.open(new Path("/user/fkong/test.log"));
 		IOUtils.copyBytes(is, System.out, 1024, true);
 	}
 }
