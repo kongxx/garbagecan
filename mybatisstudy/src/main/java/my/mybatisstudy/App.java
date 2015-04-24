0fa4f316f48997d718e127ecc9ee74293c05c983
fkong <fkong@jhinno.com>
2015-04-24 16:30:55 +0800

    new project

---
+++ mybatisstudy/src/main/java/my/mybatisstudy/App.java
@@ -0,0 +1,13 @@
+package my.mybatisstudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
