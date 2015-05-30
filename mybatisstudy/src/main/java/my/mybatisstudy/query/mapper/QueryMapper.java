88985f0787c38868f218991f44133328d531d173
fkong <fkong@jhinno.com>
2015-05-30 11:30:38 +0800

    test query

--- mybatisstudy/src/main/java/my/mybatisstudy/query/mapper/QueryMapper.java
+++ mybatisstudy/src/main/java/my/mybatisstudy/query/mapper/QueryMapper.java
@@ -4,5 +4,10 @@ import java.util.List;
 import java.util.Map;
 
 public interface QueryMapper {
+	List<Map<String, Object>> listLicenseName();
+
+	List<Map<String, Object>> listValueByLicense(Map<String, String> params);
+
 	List<Map<String, Object>> queryChartData(Map<String, Object> params);
+
 }
--- mybatisstudy/src/main/java/my/mybatisstudy/query/service/QueryService.java
+++ mybatisstudy/src/main/java/my/mybatisstudy/query/service/QueryService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,6 +14,17 @@ import java.util.Map;
 public class QueryService {
 	@Autowired
 	private QueryMapper queryMapper;
+
+	public List<Map<String, Object>> listLicenseName() {
+		return queryMapper.listLicenseName();
+	}
+
+	public List<Map<String, Object>> listValueByLicense(String licenseName, String column) {
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("license_name", licenseName);
+		params.put("column", column);
+		return queryMapper.listValueByLicense(params);
+	}
 
 	public List<Map<String, Object>> queryChartData(Map<String, Object> params) {
 		return queryMapper.queryChartData(params);
