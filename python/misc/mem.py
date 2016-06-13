d236e2517af902f1c4923b071e164612b770f1a7
kongxx <kongxx@hotmail.com>
2016-06-13 10:44:24 +0800

    python misc

---
+++ python/misc/mem.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import re
+import time
+
+def print_help():
+    print 'Usage: '
+    print '  python mem.py 100MB'
+    print '  python mem.py 1GB'
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        pattern = re.compile('^(\d*)([M|G]B)$')
+        print sys.argv[1].upper()
+        match = pattern.match(sys.argv[1].upper())
+        if match:
+            print '--'
+            num = int(match.group(1))
+            unit = match.group(2)
+            if unit == 'MB':
+                s = ' ' * (num * 1024 * 1024)
+            else:
+                s = ' ' * (num * 1024 * 1024 * 1024)
+
+            time.sleep(10000)
+        else:
+            print_help()
+    else:
+        print_help()
