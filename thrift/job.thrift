0eee6c315dadc36cfab0c63ca2dc88e17b1ddf5d
kongxx <kongxx@hotmail.com>
2016-08-09 15:55:26 +0800

    update thrift docs

--- thrift/job.thrift
+++ thrift/job.thrift
@@ -1,6 +1,6 @@
 
 struct Job {
-	1: string id,
+    1: string id,
     2: string name,
     3: string queue,
     4: string user,
@@ -8,5 +8,5 @@ struct Job {
 }
 
 service JobService {
-    list<Job> getJobs(1:i32 size)
+    list<Job> getJobs(1: i32 size)
 }
