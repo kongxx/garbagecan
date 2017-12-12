14d9f4f65d3fe3284a9c5b0eddfec0a0b1bd0fe8
kongxx <kongxx@hotmail.com>
2017-12-12 18:10:42 +0800

    ignite - transaction test

--- ignitestudy/src/main/java/my/ignitestudy/TestDataGrid.java
+++ ignitestudy/src/main/java/my/ignitestudy/TestDataGrid.java
@@ -6,25 +6,32 @@ import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
+import org.apache.ignite.transactions.Transaction;
 
 import java.util.Arrays;
 
 public class TestDataGrid {
 	public static void main( String[] args ) {
-		test();
+//		testGetPut();
+//		testAtomOperation();
+		TestTransaction();
 	}
 
-	public static void test() {
+	private static Ignite getIgnite() {
 		TcpDiscoverySpi spi = new TcpDiscoverySpi();
 		TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
-		ipFinder.setAddresses(Arrays.asList("192.168.0.192:47500..47509", "192.168.0.193:47500..47509"));
+		ipFinder.setAddresses(Arrays.asList("192.168.0.192:47500..47509"));
 		spi.setIpFinder(ipFinder);
 		IgniteConfiguration cfg = new IgniteConfiguration();
 		cfg.setDiscoverySpi(spi);
 		cfg.setClientMode(true);
 
 		Ignite ignite = Ignition.start(cfg);
-		IgniteCache<String, String> cache = ignite.getOrCreateCache("myCache");
+		return ignite;
+	}
+
+	private static void testGetPut() {
+		IgniteCache<String, String> cache = getIgnite().getOrCreateCache("myCache");
 
 		for (int i = 0; i < 10; i++) {
 			cache.put("mykey_" + i, "myvalue_" + i);
@@ -35,4 +42,60 @@ public class TestDataGrid {
 			System.out.println("Got [key=" + key + ", val=" + cache.get(key) + ']');
 		}
 	}
+
+	private static void testAtomOperation() {
+		IgniteCache<String, Integer> cache = getIgnite().getOrCreateCache("myCache");
+
+		Integer oldValue = cache.getAndPutIfAbsent("Hello", 11);
+		System.out.println("Hello: " + oldValue);
+
+		boolean success = cache.putIfAbsent("World", 22);
+		System.out.println("World: " + success);
+
+		oldValue = cache.getAndReplace("Hello", 11);
+		System.out.println("Hello replace: " + oldValue);
+
+		success = cache.replace("World", 22);
+		System.out.println("World replace: " + success);
+
+		success = cache.replace("World", 2, 22);
+		System.out.println("World replace: " + success);
+
+		success = cache.remove("Hello", 1);
+		System.out.println("World remove: " + success);
+	}
+
+	private static void TestTransaction() {
+		final Ignite ignite = getIgnite();
+		final IgniteCache<String, Integer> cache = getIgnite().getOrCreateCache("myCache");
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Begin Transaction");
+				try (Transaction tx = ignite.transactions().txStart()) {
+					Integer hello = cache.get("MyKey");
+
+					if (hello == 1) {
+						cache.put("Hello", 11);
+					}
+
+					cache.put("World", 22);
+
+					tx.commit();
+				}
+				System.out.println("End Transaction");
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+
+			}
+		}).start();
+
+
+	}
+
 }
