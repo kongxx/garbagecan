6b5704035906dd76d9583286098d266b1fe23513
kongxx <kongxx@hotmail.com>
2019-01-04 08:14:43 +0800

    support find in war files

--- tools/shell/find_class.sh
+++ tools/shell/find_class.sh
@@ -2,13 +2,23 @@
 
 find_dir=$1
 find_key=$2
-jars=`find $find_dir -name '*.jar'`
 
+jars=`find $find_dir -name '*.jar'`
 for jar in $jars
 do
     ret=`jar tvf $jar | grep $find_key`
     if [ "$?" = "0" ]; then
         ret=`echo $ret | awk '{print $8}'`
         echo -e "\e[1;34m${jar}\e[0m: \e[2;34m${ret}\e[0m"
+    fi
+done
+
+wars=`find $find_dir -name '*.war'`
+for war in $wars
+do
+    ret=`jar tvf $war | grep $find_key`
+    if [ "$?" = "0" ]; then
+        ret=`echo $ret | awk '{print $8}'`
+        echo -e "\e[1;34m${war}\e[0m: \e[2;34m${ret}\e[0m"
     fi
 done
