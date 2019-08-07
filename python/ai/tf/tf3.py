c686f6b62cec4ade28179c8c870e2156697db982
kongxx <kongxx@hotmail.com>
2019-08-07 02:06:46 -0400

    ai test

--- python/ai/tf/tf3.py
+++ python/ai/tf/tf3.py
@@ -14,9 +14,14 @@ def add_layer(inputs, in_size, out_size, activation_function=None):
     return outputs
 
 
-x_data = np.linspace(-1, 1, 300, dtype=np.float32)[:, np.newaxis]
+x_data = np.linspace(-1, 1, 300, dtype=np.float32).reshape(300, 1)
 noise = np.random.normal(0, 0.05, x_data.shape).astype(np.float32)
 y_data = np.square(x_data) - 0.5 + noise
+
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()
 
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
@@ -27,26 +32,24 @@ prediction = add_layer(l1, 10, 1, activation_function=None)
 
 loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
 # loss = tf.reduce_mean(tf.square(ys - prediction))
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.global_variables_initializer()
-
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.scatter(x_data, y_data)
-plt.ion()
-plt.show()
 
 sess = tf.Session()
 sess.run(init)
 lines = None
 for i in range(1000):
-    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    sess.run(train, feed_dict={xs: x_data, ys: y_data})
     if i % 50 == 0:
         if lines:
             ax.lines.remove(lines[0])
         prediction_value = sess.run(prediction, feed_dict={xs: x_data})
-        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+        lines = ax.plot(x_data, prediction_value, 'r-', lw=2)
         plt.pause(0.1)
         print(sess.run(loss, feed_dict={xs: x_data, ys: y_data}))
+
 sess.close()
+
+plt.ioff()
+plt.show()
