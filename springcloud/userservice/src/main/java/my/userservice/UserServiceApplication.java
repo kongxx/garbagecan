a1985f487a25a07139a88f192dfbf6740a2112f8
kongxx <kongxx@hotmail.com>
2019-11-05 23:29:16 +0800

    eureka user service

---
+++ springcloud/userservice/src/main/java/my/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package my.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
