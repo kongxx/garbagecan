c46d7378fd44b6842137b29975327c23fdde9b88
kongxx <kongxx@hotmail.com>
2019-11-05 23:21:25 +0800

    spring eureka hello service

---
+++ springcloud/helloservice/src/main/java/my/helloservice/HelloController.java
@@ -0,0 +1,13 @@
+package my.helloservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello World!";
+	}
+}
---
+++ springcloud/helloservice/src/test/java/my/helloservice/HelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.helloservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
