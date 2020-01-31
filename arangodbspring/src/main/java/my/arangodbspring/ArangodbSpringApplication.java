4ac452310ca903038b2d7408dc5934c32b7eb7eb
kongxx <kongxx@hotmail.com>
2020-01-31 14:14:26 +0800

    arangodb test

---
+++ arangodbspring/src/main/java/my/arangodbspring/ArangodbSpringApplication.java
@@ -0,0 +1,13 @@
+package my.arangodbspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArangodbSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArangodbSpringApplication.class, args);
+	}
+
+}
---
+++ arangodbspring/src/main/java/my/arangodbspring/MyConfiguration.java
@@ -0,0 +1,29 @@
+package my.arangodbspring;
+
+import com.arangodb.ArangoDB;
+import com.arangodb.Protocol;
+import com.arangodb.springframework.annotation.EnableArangoRepositories;
+import com.arangodb.springframework.config.AbstractArangoConfiguration;
+import com.arangodb.springframework.config.ArangoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableArangoRepositories(basePackages = { "my.arangodbspring" })
+public class MyConfiguration extends AbstractArangoConfiguration {
+
+	@Override
+	public ArangoDB.Builder arango() {
+		ArangoDB.Builder arango = new ArangoDB.Builder()
+				.useProtocol(Protocol.HTTP_JSON)
+				.host("localhost", 8529)
+				.user("root")
+				.password("Letmein");
+		return arango;
+	}
+
+	@Override
+	public String database() {
+		return "mydb";
+	}
+
+}
