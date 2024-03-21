696de294349c6bb342bcdff401c2ed4cb39954a0
fanbin <kongxx@hotmail.com>
2024-03-21 11:55:40 +0800

    graphql doccs

--- graphqlstudy/graphql-examples/server.js
+++ graphqlstudy/graphql-examples/server.js
@@ -2,29 +2,29 @@ const { ApolloServer } =  require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const fs = require("fs");
 
-// const typeDefs = fs.readFileSync('./query_simple/schema.graphql').toString();
-// const resolvers = require('./query_simple/resolvers');
+// const typeDefs = fs.readFileSync('./src/query_simple/schema.graphql').toString();
+// const resolvers = require('./src/query_simple/resolvers');
 
-const typeDefs = fs.readFileSync('./src/query_relation/schema.graphql').toString();
-const resolvers = require('./src/query_relation/resolvers');
+// const typeDefs = fs.readFileSync('./src/query_relation/schema.graphql').toString();
+// const resolvers = require('./src/query_relation/resolvers');
 
-// const typeDefs = fs.readFileSync('./query_pagination/schema.graphql').toString();
-// const resolvers = require('./query_pagination/resolvers');
+const typeDefs = fs.readFileSync('./src/query_pagination/schema.graphql').toString();
+const resolvers = require('./src/query_pagination/resolvers');
 
-// const typeDefs = fs.readFileSync('./query_fragment/schema.graphql').toString();
-// const resolvers = require('./query_fragment/resolvers');
+// const typeDefs = fs.readFileSync('./src/query_fragment/schema.graphql').toString();
+// const resolvers = require('./src/query_fragment/resolvers');
 
-// const typeDefs = fs.readFileSync('./query_directive/schema.graphql').toString();
-// const resolvers = require('./query_directive/resolvers');
+// const typeDefs = fs.readFileSync('./src/query_directive/schema.graphql').toString();
+// const resolvers = require('./src/query_directive/resolvers');
 
-// const typeDefs = fs.readFileSync('./mutation_simple/schema.graphql').toString();
-// const resolvers = require('./mutation_simple/resolvers');
+// const typeDefs = fs.readFileSync('./src/mutation_simple/schema.graphql').toString();
+// const resolvers = require('./src/mutation_simple/resolvers');
 
-// const typeDefs = fs.readFileSync('./mutation_inputtype/schema.graphql').toString();
-// const resolvers = require('./mutation_inputtype/resolvers');
+// const typeDefs = fs.readFileSync('./src/mutation_inputtype/schema.graphql').toString();
+// const resolvers = require('./src/mutation_inputtype/resolvers');
 
-// const typeDefs = fs.readFileSync('./scalar/schema.graphql').toString();
-// const resolvers = require('./scalar/resolvers');
+// const typeDefs = fs.readFileSync('./src/scalartype/schema.graphql').toString();
+// const resolvers = require('./src/scalartype/resolvers');
 
 const server = new ApolloServer({
   typeDefs,
--- graphqlstudy/graphql-examples/src/query_pagination/readme.md
+++ graphqlstudy/graphql-examples/src/query_pagination/readme.md
@@ -70,3 +70,35 @@ query GetUsers($sortBy: SortBy, $sort: SortDirection, $offset: Int = 0, $limit: 
   }
 }
 ```
+
+## 使用curl
+
+``` shell
+curl -X POST http://localhost:4000/ -H "Content-Type: application/json" -d '{"query":"query GetUsers($sortBy: SortBy, $sort: SortDirection, $offset: Int = 0, $limit: Int = 5) {\n  users(sortBy: $sortBy, sort: $sort, offset: $offset, limit: $limit)  {\n    id,\n    name,\n    email\n  }\n}","variables":{"sortBy":"name","sort":"ASC"},"operationName":"GetUsers"}' | json_pp
+```
+
+或者把数据保存到文件中，比如： data.json
+
+data.json
+
+``` javascript
+{
+  "query": "query GetUsers($sortBy: SortBy, $sort: SortDirection, $offset: Int = 0, $limit: Int = 5) {
+    users(sortBy: $sortBy, sort: $sort, offset: $offset, limit: $limit)  {
+      id,
+      name,
+      email
+    }
+  }",
+  "variables": {
+    "sortBy":"name",
+    "sort":"ASC"
+  }
+}
+```
+
+然后执行
+
+``` shell
+curl -X POST http://localhost:4000/ -H "Content-Type: application/json"  --data @data.json | json_pp
+```
